0c16730c038fd8bd6ae7b75e826a28abc9ab72b5
ankitgogawat <gogawat31@gmail.com>
2020-05-20 17:11:37 +0530

    day2 mathfns question

---
+++ js-exercises/math-fns/mathFns.js
@@ -0,0 +1,19 @@
+// Don't change the export values.
+// You can use the function in the Math module
+function sqrt(number) {
+  return Math.sqrt(number);
+}
+
+function power(base, exponent) {
+  return base ** exponent;
+}
+
+function round(number) {
+  return Math.floor(number);
+}
+
+export {
+  sqrt,
+  power,
+  round,
+};
